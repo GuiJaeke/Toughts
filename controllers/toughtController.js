ed88509db7ba2396dd31b232ae9e63ccc45a2bf0
GuilhermeJaekeMontanna <suporte05@montanna.com.br>
2024-09-24 17:14:11 -0300

    criação de middlewares para verificação de usuário e tela personalizada por usuário

--- controllers/toughtController.js
+++ controllers/toughtController.js
@@ -5,5 +5,31 @@ module.exports = class toughtController {
     static async showAll(req, res) {
         res.render('home')
     }
+    static async dashboard(req, res) {
+        const toughts = await tought.findAll({ raw: true })
+        res.render('dashboard', { toughts: toughts })
+    }
+    static async createTought(req, res) {
+
+        res.render('create')
+    }
+    static async createToughtPost(req, res) {
+
+        const Tought = {
+            title: req.body.title,
+            userId: req.session.userid
+        }
+        try {
+            await tought.create(Tought)
+
+            req.flash('message', 'Pensamento criado com sucesso!')
+
+            req.session.save(() => {
+                res.redirect('/toughts/dashboard')
+            })
+        } catch (err) {
+            console.log(err);
+        }
+    }
 
 }
--- routes/toughtsRoutes.js
+++ routes/toughtsRoutes.js
@@ -2,6 +2,11 @@ const express = require('express')
 const router = express.Router()
 const toughtController = require('../controllers/toughtController.js')
 
+const checkAuth = require('../helpers/auth.js').checkAuth
+
 router.get('/', toughtController.showAll)
+router.get('/add', toughtController.createTought)
+router.post('/add', toughtController.createToughtPost)
+router.get('/dashboard', checkAuth, toughtController.dashboard)
 
 module.exports = router
